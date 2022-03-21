fe46defb72fee182e3841e57383779a8da423ae5
Leandro Marasca <marasca.leandro@gmail.com>
2022-03-21 20:16:35 -0300

    se agrega lista

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ console.log("app.js started");
 import Button from "./button.js";
 import Image from "./imagen.js";
 import Sound from "./sound.js";
+import List from "./list.js";
 
 let myDiv1 = document.querySelector("#myApp");
 
@@ -10,6 +11,10 @@ let myButton = new Button("click me!");
 let myImagen = new Image("escudo", "./data/river.jpg");
 let mySound = new Sound("https://www.w3schools.com/html/horse.ogg");
 
+const listItems = ["item 1", "item 2", "item 4", "item 5", "item 3"];
+let myList = new List(listItems);
+
 myDiv1.innerHTML = myButton.render();
 myDiv1.innerHTML += myImagen.render();
 myDiv1.innerHTML += mySound.render();
+myDiv1.innerHTML += myList.render();
---
+++ js/list.js
@@ -0,0 +1,15 @@
+export default class List {
+    constructor(items) {
+        this.items=items;
+    }
+
+    render() {
+        let html = "<ul>";
+        for (let item of this.items) {
+            html += `<li>${item}</li>`;
+        }
+        html += "</ul>"
+        return html;
+    }
+
+}
